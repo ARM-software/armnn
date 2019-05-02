93e2e40500b2e0b7a47f3214a9b0499da50476b0
Sadik Armagan <sadik.armagan@arm.com>
2019-05-02 09:31:38 +0100

    IVGCVSW-2941 THIRD_PARTY_INCLUDE_DIRS is not set in armnn/tests/CMakeLists.txt for external builds
     * Added stb_image libraries under third-party/stb_image
     * Updates test/CMakeLists.txt to include those stb_image libraries
     * stb_image libraries are dual-licensed under public domain and MIT
    
    Change-Id: Ic179928598bb236d39fb5992042183099e78a0d7
    Signed-off-by: Sadik Armagan <sadik.armagan@arm.com>

--- tests/InferenceTestImage.cpp
+++ tests/InferenceTestImage.cpp
@@ -12,13 +12,13 @@
 #include <array>
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <stb/stb_image.h>
 
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <stb_image_resize.h>
+#include <stb/stb_image_resize.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
+#include <stb/stb_image_write.h>
 
 namespace
 {

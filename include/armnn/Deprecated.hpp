afe78896447a90e065df6d9b1e8753bd85525750
Derek Lamberti <derek.lamberti@arm.com>
2019-05-10 11:24:12 +0100

    IVGCVSW-2990 Add deprecation helpers
    Change-Id: Ie13aa6aee9e4da14a0ca8779c95dfd647b656f95
    Signed-off-by: Derek Lamberti <derek.lamberti@arm.com>

---
+++ include/armnn/Deprecated.hpp
@@ -0,0 +1,43 @@
+//
+// Copyright © 2017 Arm Ltd. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#pragma once
+
+#if __GNUC__
+#   define ARMNN_NO_DEPRECATE_WARN_BEGIN \
+    _Pragma("GCC diagnostic push")  \
+    _Pragma("GCC diagnostic ignored \"-Wdeprecated-declarations\"")
+
+#   define ARMNN_NO_DEPRECATE_WARN_END \
+    _Pragma("GCC diagnostic pop")
+
+#elif __clang__
+#   define ARMNN_NO_DEPRECATE_WARN_BEGIN \
+    _Pragma("clang diagnostic push")  \
+    _Pragma("clang diagnostic ignored \"-Wdeprecated-declarations\"")
+
+#   define ARMNN_NO_DEPRECATE_WARN_END \
+    _Pragma("clang diagnostic pop")
+
+#elif defined (_MSC_VER)
+#   define ARMNN_NO_DEPRECATE_WARN_BEGIN \
+    __pragma(warning( push )) \
+    __pragma(warning(disable : 4996))
+
+#   define ARMNN_NO_DEPRECATE_WARN_END \
+    __pragma(warning( pop ))
+
+#else
+#   define ARMNN_NO_DEPRECATE_WARN_BEGIN
+#   define ARMNN_NO_DEPRECATE_WARN_END
+#endif
+
+#define ARMNN_SUPRESS_DEPRECATE_WARNING(func) \
+ARMNN_NO_DEPRECATE_WARN_BEGIN \
+func; \
+ARMNN_NO_DEPRECATE_WARN_END
+
+#define ARMNN_DEPRECATED [[deprecated]]
+#define ARMNN_DEPRECATED_MSG(message) [[deprecated(message)]]

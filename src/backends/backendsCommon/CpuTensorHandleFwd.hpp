df2a9b9a978ef50752923d0d730fa36e9f6d357a
Narumol Prangnawarat <narumol.prangnawarat@arm.com>
2019-12-12 12:05:43 +0000

    IVGCVSW-4261 Add backward compatibility to the Backend API
     * Redirect the backendsCommon headers to the new headers
       in armnn/backends
    
    Signed-off-by: Narumol Prangnawarat <narumol.prangnawarat@arm.com>
    Change-Id: I53424a16b378e50a20cf1e3f7f8e9ea477fed52f

---
+++ src/backends/backendsCommon/CpuTensorHandleFwd.hpp
@@ -0,0 +1,9 @@
+﻿//
+// Copyright © 2019 Arm Ltd. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+// This file is depricated and will be removed soon.
+// Please use the new header in armnn/backends instead.
+// This will use the new armnn/backends header.
+#include <armnn/backends/CpuTensorHandleFwd.hpp>
---
+++ src/backends/backendsCommon/OptimizationViews.hpp
@@ -0,0 +1,9 @@
+//
+// Copyright © 2019 Arm Ltd. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+// This file is depricated and will be removed soon.
+// Please use the new header in armnn/backends instead.
+// This will use the new armnn/backends header.
+#include <armnn/backends/OptimizationViews.hpp>

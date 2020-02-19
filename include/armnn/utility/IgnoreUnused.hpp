9f8cad27f34f31560e55c65451fcd8d4ec2f334e
Derek Lamberti <derek.lamberti@arm.com>
2020-02-19 13:17:02 +0000

    IVGCVSW-4482 Introduce IgnoreUnused implementation
    Change-Id: Iad534e0e15ce561be6d0ffa23be19496be361385
    Signed-off-by: Derek Lamberti <derek.lamberti@arm.com>

---
+++ include/armnn/utility/IgnoreUnused.hpp
@@ -0,0 +1,16 @@
+//
+// Copyright © 2020 Arm Ltd. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#pragma once
+
+namespace armnn
+{
+
+// Utility function to selectively silence unused variable compiler warnings
+
+template<typename ... Ts>
+inline void IgnoreUnused(Ts&&...){}
+
+} //namespace armnn

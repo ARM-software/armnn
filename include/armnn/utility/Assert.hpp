dcfc63613054711d58f8bd0d9ea55ccbaf328589
Derek Lamberti <derek.lamberti@arm.com>
2020-02-19 13:25:42 +0000

    IVGCVSW-4485 Introduce ARMNN_ASSERT(_MSG) macros
    Change-Id: Iba372f7311a68877a42e52595430db212c5c9fec
    Signed-off-by: Derek Lamberti <derek.lamberti@arm.com>

---
+++ include/armnn/utility/Assert.hpp
@@ -0,0 +1,21 @@
+//
+// Copyright © 2020 Arm Ltd. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#pragma once
+
+#include <cassert>
+
+namespace armnn
+{
+
+#ifndef NDEBUG
+#   define ARMNN_ASSERT(COND) assert(COND)
+#   define ARMNN_ASSERT_MSG(COND, MSG) assert(COND && MSG)
+#else
+#   define ARMNN_ASSERT(COND)
+#   define ARMNN_ASSERT_MSG(COND, MSG)
+#endif
+
+} //namespace armnn

fdad393d5611dad69f65f9765c9472fc41cbcfe4
Nikhil Raj <nikhil.raj@arm.com>
2020-03-16 09:45:43 +0000

    Update ACL pin 0ec53a0e54ae0be0ed9c4e4c14a5fd10ed5f48a8 (COMPMID-3238)
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I035b3aeef778cd415224e9238f463bc706c00080

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename $0 )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_20_02" # Release 20.02
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="470bc1eea65560d13001e60a7f7b22b12ec89bbc" #COMPMID-3069: Improve compilation time by removing regex from test framework headers
+DEFAULT_CLFRAMEWORKREVISION="0ec53a0e54ae0be0ed9c4e4c14a5fd10ed5f48a8" #COMPMID-3238 add reference kernel for QSYMM16 Layer Normalization Kernel
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

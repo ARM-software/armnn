957b844f130066f00c8adbafae36cec2ab3e9548
Nikhil Raj <nikhil.raj@arm.com>
2020-03-24 10:00:12 +0000

    Update ACL pin to 689c968239180eda4263e34c3d450093d4a0450d
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I2f72b559594c227e2c5181fb9ec22a776ec9dcb1

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename $0 )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_20_02" # Release 20.02
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="6cd1c9bc312b65137da52a973489ba6a0f0180e3" #COMPMID-3277: (3RDPARTY_UPDATE) fix build issue related to constructors
+DEFAULT_CLFRAMEWORKREVISION="689c968239180eda4263e34c3d450093d4a0450d" #COMPMID-2967 Add support for QASYMM8_SIGNED in CLGEMMLowpQuantizeDownInt32ToUint8ScaleKernel
 
 
 usage() {

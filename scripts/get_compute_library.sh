ebc071653142efc03751d00870e64db89f6ee3ad
Nikhil Raj <nikhil.raj@arm.com>
2020-09-08 10:55:34 +0100

    Update ACL pin to e8f05da5fb919aa209e1bf0e5c70dd15fff84b7f
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I5f91150aac74547219841ea4ebe453c99760ebfc

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename $0 )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_20_08" # Release 20.08
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="ec4dee8c68a3d0f6d63db184bfb2f4589429778e" #COMPMID-3750: Disable asm kernels when shifts are negative.
+DEFAULT_CLFRAMEWORKREVISION="e8f05da5fb919aa209e1bf0e5c70dd15fff84b7f" #Add ArgMinMax layer node to Graph API.
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

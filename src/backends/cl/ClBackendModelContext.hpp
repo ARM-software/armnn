f9f33a04626756b73e6fd5c89092fd4bcb504b16
Mike Kelly <mike.kelly@arm.com>
2020-10-14 11:48:21 +0100

    IVGCVSW-5335 Added Documentation for fast_math
     * Added Documentation for fast_math to CLBackendModelContext
     * Added Documentation for fast_math to NeonBackendModelContext
    
    Signed-off-by: Mike Kelly <mike.kelly@arm.com>
    Change-Id: I43a0568ae6914e074a80130a051e5d9bb849f2ba

--- src/backends/cl/ClBackendModelContext.hpp
+++ src/backends/cl/ClBackendModelContext.hpp
@@ -9,6 +9,11 @@
 namespace armnn
 {
 
+/// The ClBackendModelContext is used to pass in CL specific backend ModelOptions. The supported backend ModelOptions
+/// are:
+///  - "FastMathEnabled"\n
+///    Using the fast_math flag can lead to performance improvements in fp32 and fp16 layers but may result in\n
+///    results with reduced or different precision. The fast_math flag will not have any effect on int8 performance.
 class ClBackendModelContext : public IBackendModelContext
 {
 public:

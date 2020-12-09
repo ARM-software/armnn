50fd30e96efd175f10883c046eb7c1fba1f6f321
Nikhil Raj <nikhil.raj@arm.com>
2020-12-09 10:11:37 +0000

    Update ACL pin to 98e33b97b92c912f058bfb3295adad1bcad3e80f
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I3c51a9a16908af2b1bc9250ada31b8ca46307fff

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename $0 )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_20_11" # Release 20.11
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="f7c5a41aebf8951ef783aa3d45f0bc4e98656e7b" #Remove support for NEIm2Col, NECol2Im, NEGEMMInterleave4x4, NEGEMMTranspose1xW
+DEFAULT_CLFRAMEWORKREVISION="98e33b97b92c912f058bfb3295adad1bcad3e80f" #Fix invalid arguments error for cl_gemmlowp* examples without arguments.
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

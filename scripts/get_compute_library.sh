92dd48a170c5fbce9f46bfcea8df97b3c3ebdccb
Nikhil Raj <nikhil.raj@arm.com>
2021-02-08 10:05:13 +0000

    Update ACL pin to dda6914c6e923187c2ca2c3bfd71677e9c9e5c68
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I0e29496698b5e7934c2b1c18740527cd9eb23ecd

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename $0 )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_20_11" # Release 20.11
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="cf87f509fc23d02c56569f794a3fb59e1b8be277" #Tweak scheduling use of SQDMULH in quantized AVG pooling
+DEFAULT_CLFRAMEWORKREVISION="dda6914c6e923187c2ca2c3bfd71677e9c9e5c68" #Update documentation for OpenCL kernel padding removal
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

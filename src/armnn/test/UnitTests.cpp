5ff15cf92b32e9564b4d101f18bc44b4096f97ae
Colm Donelan <Colm.Donelan@arm.com>
2021-05-18 14:27:33 +0100

    IVGCVSW-5964 As part of Boost removal this Windows only code is obsolete.
    This block of code to help our Windows user is no longer relevant with
    the removal of Boost.
    
    Signed-off-by: Colm Donelan <Colm.Donelan@arm.com>
    Change-Id: I0f63277cd6913b8edfbcccf3f2cd1880227c35ec

--- src/armnn/test/UnitTests.cpp
+++ src/armnn/test/UnitTests.cpp
@@ -19,49 +19,6 @@ struct ConfigureLoggingFixture
 
 BOOST_GLOBAL_FIXTURE(ConfigureLoggingFixture);
 
-// On Windows, duplicate the boost test logging output to the Visual Studio output window using OutputDebugString.
-#if defined(_MSC_VER)
-
-#include <boost/iostreams/filtering_stream.hpp>
-#include <boost/iostreams/tee.hpp>
-#include <iostream>
-#include <common/include/WindowsWrapper.hpp>
-
-using namespace boost::iostreams;
-using namespace std;
-
-struct DebugOutputSink : boost::iostreams::sink
-{
-    std::streamsize write(const char* s, std::streamsize n)
-    {
-        // The given string is not null-terminated, so we need to copy it.
-        std::string s2(s, armnn::numeric_cast<size_t>(n));
-        OutputDebugString(s2.c_str());
-        return n;
-    }
-};
-
-class SetupDebugOutput
-{
-public:
-    SetupDebugOutput()
-    {
-        // Sends the output to both cout (as standard) and the debug output.
-        m_OutputStream.push(tee(std::cout));
-        m_OutputStream.push(m_DebugOutputSink);
-
-        boost::unit_test::unit_test_log.set_stream(m_OutputStream);
-    }
-private:
-    filtering_ostream m_OutputStream;
-    DebugOutputSink m_DebugOutputSink;
-};
-
-BOOST_GLOBAL_FIXTURE(SetupDebugOutput);
-
-#endif // defined(_MSC_VER)
-
-
 BOOST_AUTO_TEST_SUITE(LoggerSuite)
 
 BOOST_AUTO_TEST_CASE(LoggerTest)

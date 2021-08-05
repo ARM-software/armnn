84e025a90dd4f98e03608ac6f9a58866eeb04687
Finn Williams <Finn.Williams@arm.com>
2021-08-05 17:29:32 +0100

    Infer tensor shapes before graph optimization
    Signed-off-by: Finn Williams <Finn.Williams@arm.com>
    Change-Id: I2a7a9b7b21848e2b3258788f06b01159f95a0d6c

--- src/armnn/Network.cpp
+++ src/armnn/Network.cpp
@@ -1594,12 +1594,12 @@ IOptimizedNetworkPtr Optimize(const INetwork& inNetwork,
     // Get the optimized graph
     Graph& optGraph = optNetObjPtr->pOptimizedNetworkImpl->GetGraph();
 
+    // Infer the tensor infos for all output slots. Throws an exception on failure
+    optGraph.InferTensorInfos();
+
     // Perform AddBroadcastReshapeLayer optimisation
     using namespace optimizations;
     Optimizer::Pass(optGraph, MakeOptimizations(AddBroadcastReshapeLayer()));
-
-    // Infer the tensor infos for all output slots. Throws an exception on failure
-    optGraph.InferTensorInfos();
 
     // Perform optimisation passes
     Optimizer::Pass(optGraph, MakeOptimizations(SquashEqualPermuteSiblings(),

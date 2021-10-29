05eccac1433a6637ef09bb30811c8c842116456d
Nikhil Raj <nikhil.raj@arm.com>
2021-10-29 09:56:36 +0100

    Update ACL pin to 1af5416917268692fcd4b34b1d7ffebd3a2aea8a
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I07a28c209a5ad7a020bfedb98873e99317a59369

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename $0 )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_21_08" # Release 21.08
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="49956ccf029ff4c1873e3a6702b5bede95d81f7a" #Fixed issue where cppflags were being set incorrectly
+DEFAULT_CLFRAMEWORKREVISION="1af5416917268692fcd4b34b1d7ffebd3a2aea8a" #Add experimental PostOp interface to ClGemmMatrixMultiplyReshapedKernel Part 1
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

615e06f54a4c4139e81e289991ba4084aa2f69d3
Finn Williams <finn.williams@arm.com>
2022-06-20 13:48:20 +0100

    IVGCVSW-6650 Refactor ExecuteNetwork
     * Remove InferenceModel
     * Add automatic IO type, shape and name configuration
     * Depreciate various redundant options
     * Add internal output comparison
    
    Signed-off-by: Finn Williams <finn.williams@arm.com>
    Change-Id: I2eca248bc91e1655a99ed94990efb8059f541fa9

---
+++ tests/ExecuteNetwork/IExecutor.hpp
@@ -0,0 +1,22 @@
+//
+// Copyright © 2022 Arm Ltd and Contributors. All rights reserved.
+// SPDX-License-Identifier: MIT
+//
+
+#pragma once
+#include <vector>
+
+/// IExecutor executes a network
+class IExecutor
+{
+public:
+    /// Execute the given network
+    /// @return std::vector<const void*> A type erased vector of the outputs,
+    /// that can be compared with the output of another IExecutor
+    virtual std::vector<const void*> Execute()  = 0;
+    /// Print available information about the network
+    virtual void PrintNetworkInfo() = 0;
+    /// Compare the output with the result of another IExecutor
+    virtual void CompareAndPrintResult(std::vector<const void*> otherOutput) = 0;
+    virtual ~IExecutor(){};
+};

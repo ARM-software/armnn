a93cddae336067e426e8c50f6a66203bac69245b
Nikhil Raj <nikhil.raj@arm.com>
2022-06-29 09:31:44 +0100

    Update ACL pin to d8804fdc4254f614e48296e5a4efef2833ba4163
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: Ifa8879bc2542e27e5923ecad637a7e6c6f6c8eb3

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_22_05" # Release 22.05
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="b1fcefddf3f59219a9d7930d607175b7e6c39347" #Implement new Elementwise Dynamic Fusion Operators: Div, Floor
+DEFAULT_CLFRAMEWORKREVISION="d8804fdc4254f614e48296e5a4efef2833ba4163" #Fix OpenCL Winograd output transform
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

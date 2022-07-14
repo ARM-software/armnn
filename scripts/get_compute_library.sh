1a7db396d262fc8c7395d973e50a003ec6d15591
Nikhil Raj <nikhil.raj@arm.com>
2022-07-14 11:20:20 +0100

    Update ACL pin to 4bfc70e31766587c951204c93a127a486e007d0c
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I8129fcd6b20ad6ec3b43f7d3b2bfcb1de45d28e6

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_22_05" # Release 22.05
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="2186aef84fe1206545f6d5bc321752044fdd1b3d" #List licenses used by other projects in README
+DEFAULT_CLFRAMEWORKREVISION="4bfc70e31766587c951204c93a127a486e007d0c" #Add Gemm MMUL Reshaped Only Rhs Support for FP32/FP16
 
 usage() {
     echo "Usage: $CMD (Use the default clframework SHA)"

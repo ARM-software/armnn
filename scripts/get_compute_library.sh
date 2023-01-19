8f7a506edc5109623858fe6d7552631f6c3401fa
Nikhil Raj <nikhil.raj@arm.com>
2023-01-19 11:07:09 +0000

    Update ACL pin to 1b6377b603c5833e52d704dc17db14f446ebc670
    * 8911: Add broadcast batched matmul validation cases
    
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I030aca89dee6431b94ac53c93d76aabaacc5b069

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_22_11" # Release 22.11
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="f800adf185e966b16385f65b9c7250766949dbe4" #8881: Implement dynamic fusion reshape operator | https://review.mlplatform.org/c/ml/ComputeLibrary/+/8881
+DEFAULT_CLFRAMEWORKREVISION="1b6377b603c5833e52d704dc17db14f446ebc670" #8911: Add broadcast batched matmul validation cases | https://review.mlplatform.org/c/ml/ComputeLibrary/+/8911
 
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out

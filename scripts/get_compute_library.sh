a724e8526e1fee378df345c26e1f45a2bd398b31
Teresa Charlin <teresa.charlinreyes@arm.com>
2023-05-03 13:03:21 +0100

    Update ACL pin to 57132943e0df00aa008b90614ea5a9fa8b2dc18a
    # Fix im2col for fast-maths mode with padding.
    
    
    Signed-off-by: Teresa Charlin <teresa.charlinreyes@arm.com>
    Change-Id: I6140f5435b81598d29a30f1f6747fd59e2304101

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_23_02" # Release 23.02
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="60ab4e66ea3cb85042035fd1aafbfea666bb4ea7" #Fix export_to_cl_image issue in the fp16 GeMM implementation
+DEFAULT_CLFRAMEWORKREVISION="57132943e0df00aa008b90614ea5a9fa8b2dc18a" #Fix im2col for fast-maths mode with padding.
 
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out

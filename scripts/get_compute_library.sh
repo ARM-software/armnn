65f9504e7381a28cda5f02c0e1bca65a1e2b90b2
Nikhil Raj <nikhil.raj@arm.com>
2023-07-28 13:19:26 +0100

    Update ACL pin to 0b23e0e6402cb18ddf621d36454cadbb73959518
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I19e7d32ab7e4cfa078e5ddd81f5abb2ac0c954fb

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_23_05" # Release 23.05
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="fab6c210b37f1fa6b3e37a2583b18f8e4b5a4f12" #9921: Design wrapper around CKW for easier writing | https://review.mlplatform.org/c/ml/ComputeLibrary/+/9921
+DEFAULT_CLFRAMEWORKREVISION="0b23e0e6402cb18ddf621d36454cadbb73959518" #9972: Add TensorOperand and declare tensor argument
 
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out

0f29328d7b2a3d52cf3cdeab9f09a75f0c67b3e5
Nikhil Raj <nikhil.raj@arm.com>
2023-10-02 14:45:08 +0100

    Update ACL pin to a396da19ee6e5c36ae07c11e4f16a6787e9bc143
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I90355ce8e979babc62143c6fe3be1c05e7d04cdc

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_23_08" # Release 23.08
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="6e56bf3b58719772111236d3b0030fbb5e8d2e16" #10399: Revise clang-format configuration
+DEFAULT_CLFRAMEWORKREVISION="a396da19ee6e5c36ae07c11e4f16a6787e9bc143" #10346: Implement Quantized Matmul T/T and T/Nt kernels using MMUL extension
 
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out

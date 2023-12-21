0587dd01272199a36210bd0ccc266185b113df75
Teresa Charlin <teresa.charlinreyes@arm.com>
2023-12-21 09:05:39 +0000

    Remove the 2 resize tests with align corners from RefEndToEnd.
    * Relates to IVGCVSW-8193 and IVGCVSW-7346
    
    Change-Id: Ieccee93672a5c73297c4ce69d1eaec588e858df0
    
    Signed-off-by: Teresa Charlin <teresa.charlinreyes@arm.com>
    Change-Id: I44df03acd348532a54b66541d91610d382a222b7

--- src/backends/reference/test/RefEndToEndTests.cpp
+++ src/backends/reference/test/RefEndToEndTests.cpp
@@ -1371,26 +1371,15 @@ TEST_CASE("RefResizeNearestNeighborEndToEndInt16NhwcTest")
     ResizeNearestNeighborEndToEnd<armnn::DataType::QSymmS16>(defaultBackends, armnn::DataLayout::NHWC);
 }
 
-TEST_CASE("RefResizeNearestNeighborEndToEndFloatAlignCornersNhwcTest")
-{
-    ResizeNearestNeighborEndToEnd<armnn::DataType::Float32>(defaultBackends, armnn::DataLayout::NHWC, true, false);
-}
-
 TEST_CASE("RefResizeNearestNeighborEndToEndFloatHalfPixelNhwcTest")
 {
     ResizeNearestNeighborEndToEnd<armnn::DataType::Float32>(defaultBackends, armnn::DataLayout::NHWC, false, true);
 }
 
-TEST_CASE("RefResizeNearestNeighborEndToEndInt8AlignCornersNhwcTest")
-{
-    ResizeNearestNeighborEndToEnd<armnn::DataType::QAsymmS8>(defaultBackends, armnn::DataLayout::NHWC, true, false);
-}
-
-TEST_CASE("TosaRefResizeNearestNeighborEndToEndInt8HalfPixelNhwcTest")
+TEST_CASE("RefResizeNearestNeighborEndToEndInt8HalfPixelNhwcTest")
 {
     ResizeNearestNeighborEndToEnd<armnn::DataType::QSymmS8>(defaultBackends, armnn::DataLayout::NHWC, false, true);
 }
-
 
 // ReverseV2
 TEST_CASE("RefReverseV2EndToEndFloat16Test")

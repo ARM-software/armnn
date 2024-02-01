94e9cec2b63d8357b8adee7afeaa7040e3a62992
Nikhil Raj <nikhil.raj@arm.com>
2024-02-01 10:49:16 +0000

    Update ACL pin to 7ab7fca87cca8775f82b0e9efec6a40975910c17
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: Ie5cd9e79c33e4f29503099aa1dfb5069926fe5c8

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_23_11" # Release 23.11
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="fb92e22c642985a5ea7906e7e7f46285d1d47718" #11004: arm_gemm: convolution: optimize convolver.hpp.
+DEFAULT_CLFRAMEWORKREVISION="7ab7fca87cca8775f82b0e9efec6a40975910c17" #11061: Fix logic in SConscript
 
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out

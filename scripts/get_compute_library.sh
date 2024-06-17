f76d4dc9a388b78a087e6ac735e664898ab5dfee
Colm Donelan <colm.donelan@arm.com>
2024-06-17 10:27:21 +0100

    Update ACL pin to cbc27ff3ebd93f9939a6c2248626b87898545069
    Signed-off-by: Colm Donelan <colm.donelan@arm.com>
    Change-Id: Ib027a36c6c10cd37fb461af29163999bd1764be7

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_24_05" # Release 24.05
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="d7230761a65ff4d559eb28945b0d4e3dfb46926f" #11677: Enable FP16 in multi_isa+v8a.
+DEFAULT_CLFRAMEWORKREVISION="cbc27ff3ebd93f9939a6c2248626b87898545069" # Fix out-of-bound memory write
 
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out

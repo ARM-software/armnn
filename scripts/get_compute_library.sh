d7b7deab831e006abc2f4b0dc9927b6e428b0931
Jonathan Clohessy <Jonathan.Clohessy@arm.com>
2025-01-27 11:57:23 +0000

    Update ACL pin to a0433be594b4b1b6ca970ca26cc7b6a834819e59
    Signed-off-by: Jonathan Clohessy <Jonathan.Clohessy@arm.com>
    Change-Id: Icb0adb78f2ef1d933620bf2b6e63a1768ce69ded

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_24_08" # Release 24.08
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="58ec95a922d554cd6e6a32d811daffbfac233f27" #test: Add thread safety tests for CpuActivation
+DEFAULT_CLFRAMEWORKREVISION="	a0433be594b4b1b6ca970ca26cc7b6a834819e59" #fix: Incorrect trademark usage in Readme for Arm(R)-Neoverse(TM) core
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out
   the pinned version of ACL based on the SHA string defined at the top of this script (DEFAULT_CLFRAMEWORKREVISION).

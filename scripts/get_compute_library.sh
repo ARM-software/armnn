07ee9c73795e9ab4930cbafdcc797ffa268d470c
Nikhil Raj <nikhil.raj@arm.com>
2025-03-05 16:33:23 +0000

    Update CL SHA to 25.02.1 RC
    Signed-off-by: Nikhil Raj <nikhil.raj@arm.com>
    Change-Id: I560f98a6f44f7e71dd797dfac2ee67ace6a6e3de

--- scripts/get_compute_library.sh
+++ scripts/get_compute_library.sh
@@ -10,7 +10,7 @@ CMD=$( basename "$0" )
 #DEFAULT_CLFRAMEWORKREVISION="branches/arm_compute_24_08" # Release 24.08
 #
 # For pinning to a revision use this:
-DEFAULT_CLFRAMEWORKREVISION="7d6fc756b2a9dfd8776be1c5c17ba45e2652c9fc" #Update release version and SONAME"
+DEFAULT_CLFRAMEWORKREVISION="e5be2cf6b525942fa7ea08106a5c0672f5667b78" #CL 25.02.1 RC"
 usage() {
   echo -e "get_compute_library.sh: Clones the Arm Compute Library (ACL) repo from the ML Platform server and checks out
   the pinned version of ACL based on the SHA string defined at the top of this script (DEFAULT_CLFRAMEWORKREVISION).
